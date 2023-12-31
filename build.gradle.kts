a39761061faf00d5af6afec35822b9d08b62d6aa
Nikolas Rist <nikolas.rist@leanix.net>
2023-12-31 16:08:57 +0100

    Backup

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
@@ -27,4 +28,12 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
