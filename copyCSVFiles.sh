7aab2b4a9991434cf58b85bc73e5e7d3cb60662d
Nikolas Rist <nikolasrist@gmail.com>
2025-04-14 09:18:03 +0200

    Harden csv copy file and improve logs

--- copyCSVFiles.sh
+++ copyCSVFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Use this script in the `asl-kalkulationen` folder to work properly.
+# Use this script in the 'asl-kalkulationen' folder to work properly.
 
 # Iterate through all items in the current directory
 echo "Start copy process for CSV files"
@@ -8,21 +8,19 @@ echo "Creating 'csv_files' folder"
 mkdir -p ./csv_files
 for dir in ./einzelpakete/*/; do
   # Check if it is a directory
-  if [ -d "$dir" ]; then
-    echo "Copying now from directory: $dir"
+  if [ -d "$dir" ] && [[ $(basename "$dir") =~ ^[0-9]{2}$ ]]; then
+    echo "About to work through folder/person: $dir"
     # Perform actions on the directory here
     for subDir in "$dir"MS_GRASE/*/; do
-      echo "Copying from SubDirectory $subDir"
+      echo "Copying from SubDirectory: $subDir"
       # Check if it is a directory
-      if [ -d "$subDir" ]; then
+      if [ -d "$subDir" ] && [[ $(basename "$subDir") =~ ^[0-9]{2}$ ]]; then
         dirName=$(basename "$dir")
         subDirName=$(basename "$subDir")
-
         srcFile="${subDir}native_space/region_analysis/region_analysis.csv"
         destFile="./csv_files/${dirName}_${subDirName}_region_analysis.csv"
-        echo "Detected sourceFile: $srcFile"
-        echo "Detected destFile: $destFile"
         if [ -f "$srcFile" ]; then
+          echo "Created target file name: $destFile"
           cp "$srcFile" "$destFile"
         else
           echo "Warning: File $srcFile does not exist!"
